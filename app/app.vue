7e22cbadbe3df7138b040f7052e92ad2ad302a8a
fajar-dev <fajarrivaldi2015@gmail.com>
2026-02-01 12:15:36 +0700

    fix: middleware global

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,10 @@
 <script setup>
 useHead({
   meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
-  link: [{ rel: 'icon', href: '/favicon.ico' }],
+  link: [
+    { rel: 'icon', href: '/favicon.ico' },
+    { rel: 'manifest', href: '/manifest.webmanifest' }
+  ],
   htmlAttrs: { lang: 'en' }
 })
 
